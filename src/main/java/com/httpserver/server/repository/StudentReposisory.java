4665a611e5fbbc2bb42b3a2d231fea143aece9ca
Alberto <albertojunior848423803@gmail.com>
2024-11-29 23:58:31 +0300

    changes

--- src/main/java/com/httpserver/server/repository/StudentReposisory.java
+++ src/main/java/com/httpserver/server/repository/StudentReposisory.java
@@ -3,8 +3,5 @@ package com.httpserver.server.repository;
 import com.httpserver.server.model.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StudentReposisory extends JpaRepository<Student, Long> {}
-
