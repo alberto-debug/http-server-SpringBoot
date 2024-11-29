9f491429b1ecdf96619a5b3c8c79d1e519ea79e2
Alberto <albertojunior848423803@gmail.com>
2024-11-30 00:30:18 +0300

    changes

---
+++ src/main/java/com/httpserver/server/controller/StudentController.java
@@ -0,0 +1,50 @@
+package com.httpserver.server.controller;
+
+import com.httpserver.server.Service.StudentService;
+import com.httpserver.server.dto.StudentDTO;
+import com.httpserver.server.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody StudentDTO studentDTO){
+        Student student = studentService.CreateStudent(studentDTO);
+        return ResponseEntity.ok(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id){
+        return studentService.getAllStudentById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody StudentDTO studentDTO) {
+        Student student = studentService.updateStudent(id, studentDTO);
+        return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
